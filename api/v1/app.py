8fcb0e3da9a066674b3cfa06ce7d016d6a0e659b
G-omar-H <omarghazi12@gmail.com>
2024-02-10 00:28:29 +0100

    5. Filter places by Amenity

--- api/v1/app.py
+++ api/v1/app.py
@@ -14,7 +14,7 @@ app = Flask(__name__)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.register_blueprint(app_views, url_prefix="/api/v1")
 
-cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
+cors = CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 
 @app.teardown_appcontext
